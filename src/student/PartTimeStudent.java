eaa695cf8d3a0905aa3af0739c3fd5683ea3de1e
BillyZys <zhaoyuns@shernet.sheridancollege.ca>
2023-02-01 17:14:43 -0500

    add part time commit

---
+++ src/student/PartTimeStudent.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package student;
+
+/**
+ *
+ * @author Yunsheng
+ */
+public class PartTimeStudent extends Student{
+    
+   private int noOfCourse;
+
+    public PartTimeStudent(int noOfCourse, String studentId, String studentName) {
+        super(studentId, studentName);
+        this.noOfCourse = noOfCourse;
+    }
+     
+    public int getNoOfCourse(){
+        return noOfCourse;
+    }
+}
